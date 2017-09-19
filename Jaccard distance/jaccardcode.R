afc295eb32c1ae9ad128501554b05ef85d6300ca
Maria Coto <mcotsar@gmail.com>
2017-09-19 16:57:31 +0200

    modify code

--- Jaccard distance/jaccardcode.R
+++ Jaccard distance/jaccardcode.R
@@ -7,9 +7,17 @@ library('ggdendro') #to create dendrograms
 library(ape) #to create different types of dendrograms based on https://rstudio-pubs-static.s3.amazonaws.com/1876_df0bf890dd54461f98719b461d987c3d.html
 library('ggtree')
 
-#install manually sudo apt-get install r-cran-ggplot2
 
 jac1= read.table("jaccardDist.txt")
+
+# load site names
+nameSites <- read.csv("sites.csv")
+rownames(jac1) <- nameSites$name
+colnames(jac1) <- nameSites$name
+
+
+#install manually sudo apt-get install r-cran-ggplot2
+
 HC = hclust(dist(jac1), method='complete')
 plot(HC, main= "dendrograma de mierder")
 
@@ -17,7 +25,7 @@ plot(HC, main= "dendrograma de mierder")
 #ggdendrogram(HT)
 
 #keep the results
-svg('resultados.svg'), width=15, height=8)    
+svg('resultados.svg', width=15, height=8)    
 plot(HC)
 dev.off()
 
@@ -26,6 +34,23 @@ dev.off()
 plot(as.phylo(HC), type = "fan")
 
 #you can change it by "radial", etc.
+
+# plots
+
+library(reshape)
+foo <- melt(as.matrix(jac1))
+ggplot(foo, aes(x=X1, y=X2, fill=value, label=round(value, 2))) + geom_raster() + geom_text()
+
+#count how many sites we have
+jur <- read.csv("baeticadata.csv", sep=";")
+jur
+str(jur)
+library(plyr)
+count(jur$site)
+
+
+
+
 
 
 
