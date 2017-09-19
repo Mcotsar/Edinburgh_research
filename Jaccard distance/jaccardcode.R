12f6ba217249c6f83f32ec1747ed27c4f6c2be82
Maria Coto <mcotsar@gmail.com>
2017-09-19 13:38:55 +0200

    modify code

--- Jaccard distance/jaccardcode.R
+++ Jaccard distance/jaccardcode.R
@@ -3,15 +3,22 @@
 library('stats')
 library('graphics')
 library('ggplot2')
+library('ggdendro') #to create dendrograms
 library('ggtree')
 
 #install manually sudo apt-get install r-cran-ggplot2
 
 jac1= read.table("jaccardDist.txt")
 HC = hclust(dist(jac1), method='complete')
-plot(HC)
+plot(HC, main= "dendrograma de mierder")
+
+#with ggdendro
+#ggdendrogram(HT)
 
 #keep the results
 svg('resultados.svg'), width=15, height=8)    
 plot(HC)
 dev.off()
+
+
+
