2a3ba742b4366c86e8054bb99f6586c94cae370c
Maria Coto <mcotsar@gmail.com>
2017-09-19 13:01:11 +0200

    add new code

--- Jaccard distance/jaccardcode.R
+++ Jaccard distance/jaccardcode.R
@@ -1,6 +1,17 @@
 ###Code to calculate hieralchical clustering#####
 
-#package required
 library('stats')
 library('graphics')
+library('ggplot2')
+library('ggtree')
 
+#install manually sudo apt-get install r-cran-ggplot2
+
+jac1= read.table("jaccardDist.txt")
+HC = hclust(dist(jac1), method='complete')
+plot(HC)
+
+#keep the results
+svg('resultados.svg'), width=15, height=8)    
+plot(HC)
+dev.off()
