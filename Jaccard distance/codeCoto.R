d40f370cf88f6f5a7ef4fe2a0985b47dde240aa7
Maria Coto <mcotsar@gmail.com>
2017-11-06 15:56:47 +0100

    add new lines

--- Jaccard distance/codeCoto.R
+++ Jaccard distance/codeCoto.R
@@ -1,17 +1,4 @@
-
-foo <- read.csv("animals.csv")
-foo
-library(dplyr)
-library(reshape2)    
-animalsMatrix <- acast(animals, id~species, value.var="values")
-library(vegan)
-zooDistance <- vegdist(animalsMatrix, method="horn")
-zooDistance
-hclust(zooDistance)
-plot(hclust(zooDistance))
-
-
-####USE WITH BAETICA DATA#####
+####Calculating BAETICA DATA#####
 
 library(stats)
 foo = read.csv("bae.csv")
@@ -19,8 +6,8 @@ library(dplyr, warn.conflicts = FALSE)
 #install.packages('dplyr', dependencies = TRUE) to install dependencies included
 #install.packages('/home/mcotsar/Downloads/dplyr_0.7.4.tar.gz', repos=NULL, type='source') to install from exterior
 library(reshape2)
-baeMatrix = acast(foo, id ~ site, value.var= "code")
-baeMatrix = acast(foo, site ~ site, value.var="code")
+#baeMatrix = acast(foo, id ~ site, value.var= "code")
+#baeMatrix = acast(foo, site ~ site, value.var="code")
 baeMatrix = acast(foo, site ~ code)
 library(vegan)
 baeDistance = vegdist(baeMatrix, method="horn")
@@ -35,4 +22,7 @@ library(reshape)
 roo <- melt(as.matrix(baeDistance))
 ggplot(roo, aes(x=X1, y=X2, fill=value, label=round(value, 2))) + geom_raster() + geom_text() #always use X1, X2
 
-
+#print the results
+pdf('resultadosnew.pdf', width=15, height=8)    
+plot(hclust(baeDistance))
+dev.off()
