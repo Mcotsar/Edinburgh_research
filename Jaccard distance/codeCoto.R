30800cf39ad0a009d7ec004789de969cf85a1547
Maria <mcotsar@gmail.com>
2018-04-18 13:02:32 +0200

    modify code

--- Jaccard distance/codeCoto.R
+++ Jaccard distance/codeCoto.R
@@ -41,3 +41,54 @@ names(mydata) #para saber el nombre de los valores (columnas)
 
 #check by different region: Hispalis, Corduba and Astigi. Download the baezone.csv
 myData= subset(foo, region %in% c("Hispalis")) 
+
+
+###plot de puntitos para hacer frequencia
+
+
+library(ggplot2)
+
+ggplot(stamp, aes(y = region, x = code)) + geom_point()
+
+#library plyr (no confuse with dplyr)
+count(stamp$code)
+
+sasa = count(stamp$code)
+idiot = sasa$freq
+
+matrix 
+
+#list of all the sites to make a matrix
+sites=sort(unique(foo$site))
+#to add the regions
+sites = unique(foo[,c("site","region")])
+
+#to order the lsi tof sites with the regions
+sites = sites[order(sites$site),]
+
+
+foo=as.matrix(table(unique(stamp[,c("site","code")])))
+write.csv(foo,"matrixfoo.csv")
+
+how to make a matrix
+
+sites$foo = rowSums(matrix)
+
+ggplot(sites, aes(y=reorder(region, foo, FUN=sum), x=foo, fill=region)) + geom_jitter(col="grey50", alpha=0.5, shape=21, height=0.2, width=0.3, size=3) + scale_colour_manual(values=myPalette) + theme_bw() + theme(legend.position="None") + xlab("number of code stamps") + ylab("region")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
